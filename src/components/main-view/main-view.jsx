281988f06014c172223a4fb20654b911bce0993d
ElenaUJ <elena-ha@web.de>
2023-02-09 10:40:47 -0500

    Replaced hardcoded movies with empty array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,41 +5,7 @@ import { MovieView } from '../movie-view/movie-view.jsx';
 // Function returns visual representation of component
 function MainView() {
   // Empty arry is initial value of movies (state variable); usetMovies is a method to update movies variable seState() returns array of paired values that are desrtructured
-  const [movies, setMovies] = useState([
-    {
-      _id: 1,
-      Title: 'Ice Age',
-      Description:
-        'Ice Age is a movie about a group of animals trying to return a human baby to its tribe during the ice age.',
-      Genre: 'Animation',
-      Director: 'Chris Wedge',
-      ImagePath:
-        'https://m.media-amazon.com/images/M/MV5BOGEwMTQyMDktMWUwZC00MzExLTg1MGMtYWJiNWNhMzIyMGU5XkEyXkFqcGdeQXVyOTYyMTY2NzQ@._V1_.jpg',
-      Featured: false,
-    },
-    {
-      _id: 2,
-      Title: 'The Truman Show',
-      Description:
-        'The Truman Show is a movie about a man who discovers his entire life is actually a reality TV show broadcast to the world and must decide whether to continue living in the fabricated world or leave it to discover the truth of his existence.',
-      Genre: 'Drama',
-      Director: 'Peter Weir',
-      ImagePath:
-        'https://m.media-amazon.com/images/M/MV5BMDIzODcyY2EtMmY2MC00ZWVlLTgwMzAtMjQwOWUyNmJjNTYyXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
-      Featured: false,
-    },
-    {
-      _id: 3,
-      Title: 'Dead Poets Society',
-      Description:
-        'Dead Poets Society is a movie about an English teacher at a conservative, all-boys school in the 1950s who inspires his students through his teachings of poetry and self-expression.',
-      Genre: 'Drama',
-      Director: 'Peter Weir',
-      ImagePath:
-        'https://m.media-amazon.com/images/M/MV5BOGYwYWNjMzgtNGU4ZC00NWQ2LWEwZjUtMzE1Zjc3NjY3YTU1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
-      Featured: false,
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   // Default: no movie is selected
   const [selectedMovie, setSelectedMovie] = useState(null);
